9f609ade94cdb485d7021bda869d9c7d460ecb6d
Safayethossain95 <safayet.hossain95@gmail.com>
2020-11-15 22:37:56 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+$(function(){
+   
+    $("#typed").typed({
+			strings: ["Vann", "a web designer", "a web developer"],
+			typeSpeed: 30,
+            backSpeed: 40,
+            loop: true
+		});
+   
+    
+//      var html_body = $('html, body');
+//    $('nav a').on('click', function () {
+//        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+//            var target = $(this.hash);
+//            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+//            if (target.length) {
+//                html_body.animate({
+//                    scrollTop: target.offset().top + 5
+//                }, 1000);
+//                return false;
+//            }
+//        }
+//    });
+    
+});
